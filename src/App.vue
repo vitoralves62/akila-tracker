9b3c0c8a7b3893bd87fbfe4b50807810d614df4e
Vitor Alves <vitor.dev.alves@gmail.com>
2025-12-07 23:50:11 -0300

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<main class="columns is-gapledd is multiline">
+		<div class="column is-one-quarter">
+			<side-bar/>
+		</div>
+		<div class="column is-three-quarter">
+
+		</div>
+	</main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+
+import sideBar from '@/components/use-cases/side-bar.vue';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+	name: 'App',
+	components: {
+		sideBar,
+	}
+	});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped> 
+
 </style>
---
+++ src/components/use-cases/side-bar.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>
+            <img src="@/assets/logo.png" alt="" class="logo-image"/>
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'side-bar'
+    })
+</script>
+
+<style scoped>
+header {
+    padding: 1rem;
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+}
+.logo-image{
+    width: 200px !important; 
+}
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
